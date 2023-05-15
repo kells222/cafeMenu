165c31463cdaa4af917820d648bf539f1eac683b
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-05-15 11:27:46 -0700

    closer

--- coffeeData.js
+++ coffeeData.js
@@ -54,6 +54,9 @@ const coffeeMenu = [
         seasonal: true,
     }
 ]
+module.exports = coffeeMenu
+
+
 
 // Print an array of all the drinks on the menu.
 for (let i = 0; i < coffeeMenu.length; i++) {
@@ -61,14 +64,45 @@ for (let i = 0; i < coffeeMenu.length; i++) {
   }
 
 // 3. Print an array of drinks that cost 5 and under.
+let drinksLess = []
 for (let i = 0; i < coffeeMenu.length; i++) {
-console.log(coffeeMenu[i].price<=5);
-return coffeeMenu.price
+if (coffeeMenu[i].price<=5) {
+    drinksLess.push(coffeeMenu[i].name)
 }
+}
+console.log(drinksLess)
 
 // 4. Print an array of drinks that are priced at an even number.
-// 5. Print the total if you were to order one of every drink.
-// 6. Print an array with all the drinks that are seasonal.
-// 7. Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
+let evenNum =[]
+for(let i =0; i<coffeeMenu.length;i++){
+    if (coffeeMenu[i].price%2===0){
+        evenNum.push(coffeeMenu[i].name)
+    }
+}
+console.log(evenNum)
 
-// module.exports = coffeeMenu
+
+// 5. Print the total if you were to order one of every drink.
+
+// 6. Print an array with all the drinks that are seasonal.
+let season = []
+for(let i=0;i<coffeeMenu.length;i++){
+    if(coffeeMenu[i].seasonal===true){
+        season.push(coffeeMenu[i].name)
+    }
+}
+console.log(season)
+
+
+// 7. Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
+// not done
+let seasons = []
+for(let i=0;i<coffeeMenu.length;i++){
+    if(coffeeMenu[i].seasonal===true){
+        console.log(coffeeMenu.name + " with imported beans");
+    }
+}
+console.log(seasons)
+
+
+
